3a4676d75ea3c289a88c34a6840068c4be2de7cd
YuXiang <yuxiangchina@qq.com>
2022-04-27 20:22:35 -0700

    reset

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 int main() {
-    std::cout << "Hello World555666777888!\n";
+    std::cout << "Hello World!\n";
     std::vector<int> int_vec = std::vector<int>{1,2,3};
     return 0;
 }
