4f32257f3913b44af7b5e2c53706de0e430101a0
YuXiang <yuxiangchina@qq.com>
2022-04-27 20:40:43 -0700

    add pybind

---
+++ b
@@ -0,0 +1,4 @@
+pushd build
+cmake .. -DMAKE_BUILD_TYPE=Debug
+make -j
+popd
--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 int main() {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello Pybind!\n";
     std::vector<int> int_vec = std::vector<int>{1,2,3};
     return 0;
 }
